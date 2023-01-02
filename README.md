a38cf962830e1dbaa60350597b76c74189937192
sam <sam20060223@gmail.com>
2023-01-02 22:24:47 +0800

    V1.32

--- README.md
+++ README.md
@@ -1,14 +1,22 @@
 # A magic wand control.
-## **Version 1.3**
+## **Version 1.32**
 ### 目前版本功能:
 1. arduino讀取mpu6050數據並卡爾曼濾波取得yaw、pitch、roll三值
 2. 可利用python讀取arduino數據並將位移數據圖像顯示出來
+3. 延後讀取數據抵銷按按鈕時震動
+4. 數據圖像點放大以利之後圖像辨識
 ### 修復: 
 1. V1.0時python讀取序列阜數據卡在暫存區延遲過高問題(優化至10hz讀取率)
 ### 新增:
 1. 數據圖像顯示
-### 目前成果展示(V1.30):
-[![](https://img.onl/L12P2h)](https://photos.app.goo.gl/uSvbn7ETcRqxsse37)
+### 待實現:
+1. 將python程式碼翻譯至arduino-c(因目前演算法測試階段使用python可免去編譯燒錄時間提高研發效率)
+2. 實現魔仗軌跡圖像辨識，將於目前程式碼移植至esp32時再實現(因python序列阜更新率過低直接於esp32
+上使用原始資料效果更佳)
+3. 新增紅外線發射器
+4. 新增震動引擎，強化使用體驗
+### 目前成果展示(V1.32):
+[![](https://img.onl/L12P2h)](https://photos.app.goo.gl/5FmcTnXPaZoideAj7)
 # A RGB pwm LED cube.
 ### **測試中的digital模擬LEDpwm代碼**
 ### updat : 2022/12/26
--- wond/toppoint_tracker.py
+++ wond/toppoint_tracker.py
@@ -1,11 +1,11 @@
-import serial
-import keyboard
+import time
 import math
-from matplotlib import pyplot as plt
-import asyncio
 import numpy as np
+import asyncio
+import keyboard
+import serial
+from matplotlib import pyplot as plt
 import cv2
-
 
 ser = serial.Serial("COM5", 115200)
 plt.ion()
@@ -13,13 +13,13 @@ first = True
 x,y = 0,0
 coordinate = [[(0,0)]]
 button_state = True
+button_time = 0
 
 Long = 0.7 
 sl = 32 #picture side length
+point_size = 3  #px
 
 x_max,x_min,y_max,y_min = 0,sl,0,sl
-# picture = [255 for _ in range(sl*sl)]
-# picture = np.resize(picture,(sl,sl,3))
 coordinate = []
 
 async def show_picture(coordinate):
@@ -30,11 +30,20 @@ async def show_picture(coordinate):
     y_mid = (y_max+y_min)/2
     x_gap = 0-x_mid
     y_gap = 0-y_mid
+    x_size = sl/(((x_max+x_gap)))
+    y_size = sl/(((y_max+y_gap)))
+    print(x_max,x_gap,x_size)
     for c in coordinate:
         if c[0] <= sl and c[1] <= sl and c[0] >= sl*-1 and c[1] >= sl*-1:
-            cX = int((c[0]+x_gap)/2+sl/2)
-            cY = int((c[1]+y_gap)/2+sl/2)
-            picture[cX][cY] = [0,0,0]
+            cX = int(((c[0]+x_gap))*x_size/2+sl/2-point_size/2)
+            cY = int(((c[1]+y_gap))*y_size/2+sl/2-point_size/2)
+            x_num = range((point_size-int(point_size/2+0.5))*-1,int(point_size/2)+1)
+            y_num = x_num
+            for X in x_num:
+                for Y in y_num:
+                    if cX+X < sl and cX+X >= 0:
+                        if cY+Y < sl and cY+Y >= 0:
+                            picture[cX+X][cY+Y] = [0,0,0]
     coordinate = []
     print(picture)
     cv2.imwrite('picture.jpg',picture)
@@ -44,38 +53,37 @@ async def show_picture(coordinate):
     cv2.waitKey(0)
 
 async def draw():
-    global x,y,Long,value,coordinate,button_state,picture,x_max,x_min,y_max,y_min
+    global x,y,Long,value,coordinate,button_state,picture,x_max,x_min,y_max,y_min,button_time
     x = float(value[2])
     y = float(value[1])
     x = Long*(math.degrees(math.asin(math.radians(x))))
     y = Long*(math.degrees(math.asin(math.radians(y))))
     if value[4][0] == "p" and button_state == True:
+        button_time = time.time()
         start_draw = False
         button_state = False
         coordinate = []
-        # picture = [255 for _ in range(sl*sl)]
-        # picture = np.resize(picture,(sl,sl,3))
     elif value[4][0] == "s" and button_state == False:
         start_draw = True
         button_state = True
     else:
         start_draw = False
-    coordinate.append((x,y))
-    if x > x_max:
-        x_max = x
-    elif x < x_min:
-        x_min = x
-    else:
-        x_max,x_min = x_max,x_min
-    if y > y_max:
-        y_max = y
-    elif y < y_min:
-        y_min = y
-    else:
-        y_max,y_min = y_max,y_min
-    # print(picture)
-    # picture[int(x/2+sl/2)][int(y/2+sl/2)] = [0,0,0]
-    coordinate.append((x,y))
+    now_time = time.time()
+    if now_time - button_time >= 0.2:
+        coordinate.append((x,y))
+        if x > x_max:
+            x_max = x
+        elif x < x_min:
+            x_min = x
+        else:
+            x_max,x_min = x_max,x_min
+        if y > y_max:
+            y_max = y
+        elif y < y_min:
+            y_min = y
+        else:
+            y_max,y_min = y_max,y_min
+    # coordinate.append((x,y))
     if start_draw == True:
         taskP = asyncio.create_task(show_picture(coordinate))
         await taskP

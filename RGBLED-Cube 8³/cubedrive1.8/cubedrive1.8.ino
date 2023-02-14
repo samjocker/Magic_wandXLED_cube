dbe6ce375d59fbe466a9d86bac266c6b93f456ef
ivan17lai <34911327+ivan17lai@users.noreply.github.com>
2023-02-14 23:20:57 +0800

    Add files via upload

---
+++ RGBLED-Cube 8³/cubedrive1.8/cubedrive1.8.ino
@@ -0,0 +1,115 @@
+/* note 1.7 2023/2/11 By Yihuan
+digital 2~13  PWM to RGB
+digital 22~37 digital to GND
+*/
+
+
+int rgb_color_set[16][12]  = 
+                 {{255,0,0,0,0,0,0,0,0,255,0},
+                  {255,0,255,0,0,0,0,0,0,255,0},
+                  {255,0,0,0,0,0,0,0,0,255,0},
+                  {255,0,0,0,0,0,0,0,0,255,0},
+                  {255,0,0,0,0,0,0,0,0,255,0},
+                  {255,0,0,0,0,0,0,0,0,255,0},
+                  {255,0,0,0,0,0,0,0,0,255,0},
+                  {255,0,0,0,0,0,0,0,0,255,0},
+                  {255,0,0,0,0,0,0,0,0,255,0},
+                  {255,0,0,0,0,0,0,0,0,0,0},
+                  {255,0,0,0,0,0,0,0,0,0,0},
+                  {255,0,0,0,0,0,0,0,0,0,0},
+                  {255,0,0,0,0,0,0,0,0,0,0},
+                  {255,0,0,0,0,0,0,0,0,0,0},
+                  {255,0,0,0,0,0,0,0,0,0,0},
+                  {255,0,0,0,0,0,0,0,0,0,0}};
+
+void setup() {
+
+  for(int i=2;i<14;i++){
+    pinMode(i,OUTPUT);
+  }
+  for(int i=22;i<38;i++){
+    pinMode(i,OUTPUT);
+  }
+
+  led_all_gnd_control(1);
+  Serial.begin(2000000);
+}
+
+unsigned int timer=0;
+int open_now = 0;
+
+void loop() {
+  
+
+  led_one_gnd_control(timer%16);
+  led_color_show(timer%16);
+  timer++;
+  led_color_clear();
+  serial_read();
+  //Serial.println(rgb_color_set[0][1]);
+  
+}
+
+
+//control function 1.1 2023/2/9
+
+void led_all_gnd_control(int set){
+  for(int i=22;i<38;i++){
+    digitalWrite(i,set);
+  }
+}
+
+void led_one_gnd_control(int list){
+  for(int i=22;i<38;i++){
+    //Serial.println(list == i-22);
+    if(list == i-22){
+      digitalWrite(i,0);
+      //open_now = i;
+    }else{
+      digitalWrite(i,1);
+    }
+    
+  }
+}
+
+void led_color_clear(){
+  for(int i=2;i<14;i++){
+    analogWrite(i,0);
+  }
+}
+
+void led_color_show(int list){
+    for(int i=2;i<14;i++){
+      analogWrite(i,rgb_color_set[list][i-2]);
+    }
+}
+
+void rgb_list_write(){
+  
+}
+//contorl brige 1.0 2023 2/10
+
+
+String serial_input;
+
+void serial_read(){
+  if (Serial.available()) {
+    serial_input = Serial.readStringUntil('\n');
+    
+    int save_list = serial_input.substring(0,2).toInt();
+    int save_color[12];
+    //Serial.println(save_list);
+    for(int i=0;i<12;i++){
+      rgb_color_set[save_list][i] = serial_input.substring(i*3-1+3,i*3+1+2+2).toInt();
+      Serial.print(serial_input.substring(i*3-1+3,i*3+1+2+2));
+      Serial.print(" ");
+      Serial.println(rgb_color_set[0][i]);
+  
+      //Serial.println(serial_input.substring(i*3+2,i*3+2+3).toInt());
+    }
+    //print_arr(rgb_color_set[0]);
+    //rgb_color_set[save_list] = save_color;
+    
+  }
+  
+}
